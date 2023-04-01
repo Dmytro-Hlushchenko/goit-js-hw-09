538cd814ed8918c1107e124472d7c4e15b872261
Dmytro-Hlushchenko <103310889+Dmytro-Hlushchenko@users.noreply.github.com>
2023-04-01 10:25:49 +0000

    Deploying to gh-pages from @ Dmytro-Hlushchenko/goit-js-hw-09@4bc223006e51bc7a4543694faa78daf01f3f35e5 🚀

---
+++ 01-color-switcher.b94b6a67.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){r=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),e.setAttribute("disabled","")})),o.addEventListener("click",(function(){clearInterval(r),e.removeAttribute("disabled")}));let r=null;
+//# sourceMappingURL=01-color-switcher.b94b6a67.js.map
